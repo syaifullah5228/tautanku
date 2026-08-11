7f47b1be8787d1243fe93628de4c6204bc03b453
syaifullah5228 <gfxtktk5228@gmail.com>
2026-08-11 18:53:13 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{padding: 40, fontFamily: 'sans-serif'}}>
+      <h1>tautanku jalan!</h1>
+      <p>Web berhasil deploy.</p>
+    </main>
+  );
+}
